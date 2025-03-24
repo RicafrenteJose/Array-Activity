1963820b0bca3527bf44548d943b1282d4513427
RicafrenteJose <198441580+RicafrenteJose@users.noreply.github.com>
2025-03-24 21:33:12 +0800

    Add files via upload

---
+++ ArrayActivity.js
@@ -0,0 +1,25 @@
+// Create an array called favSingers and store three favorite singers
+let favSingers = ["Arthur Nery", "Daniel Ceasar", "Justin Beaber"];
+
+// Log the first singer in the array
+console.log(favSingers[0]);
+
+// Create an favorite number
+let favNumber = [4,2,19,23];
+
+// create a mix array with diff data types
+let mixedArr = ["name", ["othername"], 456, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
+
+
+
+
+
+
+
+
+
